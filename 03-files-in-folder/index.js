c0e40f616dd14ee0070c81e0e493ff8f8294be44
Winston4308 <112640916+Winston4308@users.noreply.github.com>
2022-11-08 01:26:37 +0200

    fix: corrected the mistake

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,21 +1,32 @@
-const fs = require('fs');
+let fs = require('fs');
 const path = require('path');
+const folder = '03-files-in-folder/secret-folder';
 
-fs.readdir('03-files-in-folder/secret-folder', { withFileTypes: true }, (err, files) => {
+fs.readdir(folder, { withFileTypes: true }, (err, files) => {
     if (err) {
-      console.log(err);
+        console.log(err);
     }
     else {
+        const lst = [];
         let extension;
-        let name;
+        let file_name;
 
-        for (const i of files) {
-            if (i.isFile()) {
-                extension = path.extname(i.name);
-                name = path.basename(i.name, extension);
-                console.log(`${name} - ${extension.slice(1)}`);
+        for (let file of files){
+            if (file.isFile()) {
+                extension = path.extname(file.name);
+                file_name = path.basename(file.name, extension);
+                lst.push([file.name, file_name, extension])
             }
         }
-        
+        for (let i = 0; i < lst.length; i++) {
+            fs.stat(`${folder}/${lst[i][0]}`, (errStat, status) => {
+                if (errStat) {
+                    console.log(errStat);
+                }
+                else {
+                    console.log(`${lst[i][1]} - ${lst[i][2].slice(1)} - ${status.size}`);
+                }
+            })
+        }
     }
-})
+});
